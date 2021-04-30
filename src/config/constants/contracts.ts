be4c4a726c1f73bddff5ce6aa56518903994eab0
unknown <usman.maliknu13@gmail.com>
2021-04-30 01:14:00 -0700

    first commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0x6845f66665e319a7f00a536a15930af90aab2466',
+    97: '',
+  },
+  masterChef: {
+    56: '0x0b127304ea53e43e95399c38a0b542d2f5cdda8b',
+    97: '0x995dDC209D2Fa37A0F4F7C373ae825cfa730CA0f',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
